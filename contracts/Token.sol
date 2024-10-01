4b705a021bb7d6e98cb20cb881f76206c78e36da
samblaha-mri <sam.blaha@mrisoftware.com>
2024-10-01 15:38:08 -0400

    6. Token Approvals

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,29 +1,46 @@
-// SPDX-License-Identifier: MIT 
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import 'hardhat/console.sol';
+import "hardhat/console.sol";
 
 contract Token {
     string public name;
     string public symbol;
     uint256 public decimals = 18;
     uint256 public totalSupply;
+
     // Track Balances
     mapping(address => uint256) public balanceOf;
+    /*
+    This mapping ties to the test "allocates an allowance..."
+    The furst mapping is deployer.address
+    The second mapping is exchange.address. The spender.
+     */
+    mapping(address => mapping(address => uint256)) public allowance;
 
     event Transfer(address indexed from, address indexed to, uint256 value);
 
-    constructor(string memory _name, string memory _symbol, uint256 _totalSupply) {
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint256 _totalSupply
+    ) {
         name = _name;
         symbol = _symbol;
-        totalSupply = _totalSupply * (10**decimals);
+        totalSupply = _totalSupply * (10 ** decimals);
         balanceOf[msg.sender] = totalSupply;
     }
 
-    function transfer(address _to, uint256 _value) 
-    public 
-    returns (bool success) 
-    {
+    function transfer(
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
         // Require sender has enough tokens to send
         require(balanceOf[msg.sender] >= _value);
         require(_to != address(0));
@@ -34,6 +51,19 @@ contract Token {
 
         // Emit Event
         emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function approve(
+        address _spender,
+        uint256 _value
+    ) public returns (bool success) {
+        require(_spender != address(0));
+        // msg sender is always the first address
+        allowance[msg.sender][_spender] = _value;
+
+        emit Approval(msg.sender, _spender, _value);
+
         return true;
     }
 }
